6b1d2ed5d519ae65e5062be4dd1fdd97767533f6
dcadata <39072469+dcadata@users.noreply.github.com>
2021-04-10 14:44:34 -0400

    partial rewrite

--- task.py
+++ task.py
@@ -1,77 +1,74 @@
 from argparse import ArgumentParser
+from time import sleep
 from traceback import format_exc
 import pandas as pd
 from bs4 import BeautifulSoup as BS
 from requests import get
 
-DATA_DIR = 'data/'
-
 
 class Scraper:
-    @property
-    def _soup(self):
-        r = get('https://www.michigan.gov/coronavirus/0,9753,7-406-98163_98173---,00.html', timeout=20)
-        return BS(r.text, 'lxml')
+    _page_filepath = 'data/page.html'
+    _cases_filename = 'Cases and Deaths by County by Date of Onset of Symptoms and Date of Death.xlsx'
+    _tests_filename = 'Diagnostic Tests by Result and County.xlsx'
 
-    @property
-    def _link_info(self):
-        link_info = {}
-
-        datasets_header = self._soup.find('h5', text=lambda x: str(x).strip().startswith('Public Use Datasets'))
-        datasets_links = datasets_header.find_parent().find_all('a')
-
-        for link in datasets_links:
-            if link.get('href'):
-                link_info[link.text] = 'https://www.michigan.gov' + link['href']
-
-        return link_info
-
-
-class Downloader(Scraper):
-    def __init__(self):
-        self._links = None
-        self._cases = None
-        self._tests = None
-
-    def read_files_from_disk(self):
-        _read = lambda link_text: pd.read_csv(DATA_DIR + link_text + '.csv')
-
-        self._cases = _read('Cases and Deaths by County by Date')
-        self._tests = _read('Diagnostic Tests by Result and County')
+    def make_request_to_main_page(self):
+        r = get('https://www.michigan.gov/coronavirus/0,9753,7-406-98163_98173---,00.html')
+        open(self._page_filepath, 'wb').write(r.content)
+        sleep(2)
 
     def download_remote_files(self):
-        self._links = self._link_info.copy()
+        links = self._get_links_to_remote_files()
+        for fn in (self._cases_filename, self._tests_filename):
+            r = get(links[fn.rsplit('.', 1)[0]])
+            open('data/' + fn, 'wb').write(r.content)
+            sleep(2)
 
-        self._cases = self._download_remote_excel_file('Cases and Deaths by County by Date')
-        self._tests = self._download_remote_excel_file('Diagnostic Tests by Result and County')
+    def _get_links_to_remote_files(self):
+        soup = BS(open(self._page_filepath).read(), 'lxml')
+        datasets_links = soup.find('h5', text=lambda x: str(x).strip().startswith(
+            'Public Use Datasets')).find_parent().find_all('a')
 
-    def _download_remote_excel_file(self, link_text):
-        df = pd.read_excel(self._get_remote_excel_file_url(link_text))
+        links = {}
+        for link in datasets_links:
+            if link.get('href'):
+                link_text = ' '.join(link.text.split())
+                links[link_text] = 'https://www.michigan.gov' + link['href']
 
+        return links
+
+
+class Processor(Scraper):
+    def process_and_save_remote_files(self):
+        cases = self._read_local_excel_files(self._cases_filename)
+        tests = self._read_local_excel_files(self._tests_filename)
+
+        cases.to_csv('cases.csv', index=False)
+        tests.to_csv('tests.csv', index=False)
+
+    def _read_local_excel_files(self, fn):
+        df = pd.read_excel('data/' + fn)
+        return self._modify_dataframe(df)
+
+    @staticmethod
+    def _modify_dataframe(df):
         cols = {}
         for col in df.columns:
             cols[col] = col.lower().replace('.', '_')
-
         cols['MessageDate'] = 'date'
         df = df.rename(columns=cols)
 
-        df['date'] = df['date'].apply(lambda x: pd.to_datetime(x).date())
-        df.to_csv(DATA_DIR + link_text + '.csv', index=False)
+        df.date = df.date.apply(lambda x: pd.to_datetime(x).date())
         return df
 
-    def _get_remote_excel_file_url(self, link_text):
-        url = self._links.get('link_text')
-        if not url:
-            for text, link in self._links.items():
-                if text.startswith(link_text):
-                    return link
-        return url
 
-
-class Roller(Downloader):
+class Roller(Processor):
     @property
-    def _counties(self):
-        return self._cases['county'].unique()
+    def _cases(self):
+        return pd.read_csv('cases.csv')
+
+    @property
+    def _tests(self):
+        return pd.read_csv('tests.csv')
 
     @property
     def cases_rolling(self):
@@ -83,18 +80,22 @@ class Roller(Downloader):
         tests['positive_rate'] = tests['positive'] / tests['total']
         return self._create_df_with_rolling(tests, ['negative'], ['positive_rate'])
 
+    @property
+    def _counties(self):
+        return self._cases['county'].unique()
+
     def _create_df_with_rolling(self, df, drop_cols, roll_cols):
         df = df.drop(columns=drop_cols)
         if 'updated' in df.columns:
             df = df.drop(columns='updated')
-        return pd.concat([_add_rolling_averages(df, county, roll_cols) for county in self._counties], sort=False)
+        return pd.concat([self._add_rolling_averages(df, county, roll_cols) for county in self._counties])
 
-
-def _add_rolling_averages(df, county, roll_cols):
-    df = df[df['county'] == county].copy()
-    for col in roll_cols:
-        df[f'{col}_roll'] = df[col].rolling(7).mean()
-    return df
+    @staticmethod
+    def _add_rolling_averages(df, county, roll_cols):
+        df = df[df['county'] == county].copy()
+        for col in roll_cols:
+            df[f'{col}_roll'] = df[col].rolling(7).mean()
+        return df
 
 
 def run():
@@ -105,9 +106,9 @@ def run():
 
     roller = Roller()
     if args.r:
+        roller.make_request_to_main_page()
         roller.download_remote_files()
-    else:
-        roller.read_files_from_disk()
+        roller.process_and_save_remote_files()
 
     tests_rolling = roller.tests_rolling.copy()
     if args.m:
@@ -115,8 +116,8 @@ def run():
         for key in ('date', 'positive_rate', 'positive_rate_roll'):
             print(f'{key}: {tr[key]}')
     else:
-        roller.cases_rolling.to_csv(DATA_DIR + 'cases_roll.csv', index=False)
-        tests_rolling.to_csv(DATA_DIR + 'tests_roll.csv', index=False)
+        roller.cases_rolling.to_csv('cases_roll.csv', index=False)
+        tests_rolling.to_csv('tests_roll.csv', index=False)
 
 
 def main():
